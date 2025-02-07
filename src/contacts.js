7b636ae861627f7b9be1578ea4b14e0ca1cd10f3
Vasyl Protsukovych <protcykovich@gmail.com>
2025-02-07 23:29:21 +0000

    Applied another prettier rules and remove node modules from repo

--- src/contacts.js
+++ src/contacts.js
@@ -8,37 +8,35 @@ const __dirname = path.dirname(__filename);
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 
 export async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    console.error('Error reading contacts: ', error.message);
-    return [];
-  }
+    try {
+        const data = await fs.readFile(contactsPath, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading contacts: ', error.message);
+        return [];
+    }
 }
 
 export async function getContactById(contactId) {
-  const contacts = await listContacts();
-  return contacts.find((contact) => contact.id === contactId) || null;
+    const contacts = await listContacts();
+    return contacts.find((contact) => contact.id === contactId) || null;
 }
 
 export async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const contactIndex = contacts.findIndex(
-    (contact) => contact.id === contactId
-  );
-  if (contactIndex === -1) {
-    return null;
-  }
-  const [removedContact] = contacts.splice(contactIndex, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return removedContact;
+    const contacts = await listContacts();
+    const contactIndex = contacts.findIndex((contact) => contact.id === contactId);
+    if (contactIndex === -1) {
+        return null;
+    }
+    const [removedContact] = contacts.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
 }
 
 export async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const newContact = { id: Date.now().toString(), name, email, phone };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+    const contacts = await listContacts();
+    const newContact = { id: Date.now().toString(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
 }
--- src/index.js
+++ src/index.js
@@ -1,58 +1,49 @@
 import { program } from 'commander';
-import {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-} from './contacts.js';
+import { listContacts, getContactById, addContact, removeContact } from './contacts.js';
 
 program
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
 
 program.parse();
 
 const options = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
-  try {
-    switch (action) {
-      case 'list':
-        const contacts = await listContacts();
-        console.table(contacts);
-        break;
+    try {
+        switch (action) {
+            case 'list':
+                const contacts = await listContacts();
+                console.table(contacts);
+                break;
 
-      case 'get':
-        if (!id) throw new Error('Please provide a contact ID.');
-        const contact = await getContactById(id);
-        console.log(contact || 'Contact not found');
-        break;
+            case 'get':
+                if (!id) throw new Error('Please provide a contact ID.');
+                const contact = await getContactById(id);
+                console.log(contact || 'Contact not found');
+                break;
 
-      case 'add':
-        if (!name || !email || !phone)
-          throw new Error('Please provide name, email, and phone.');
-        const newContact = await addContact(name, email, phone);
-        console.log('Added contact:', newContact);
-        break;
+            case 'add':
+                if (!name || !email || !phone) throw new Error('Please provide name, email, and phone.');
+                const newContact = await addContact(name, email, phone);
+                console.log('Added contact:', newContact);
+                break;
 
-      case 'remove':
-        if (!id) throw new Error('Please provide a contact ID.');
-        const removedContact = await removeContact(id);
-        console.log(
-          removedContact ? 'Removed contact:' : 'Contact not found',
-          removedContact
-        );
-        break;
+            case 'remove':
+                if (!id) throw new Error('Please provide a contact ID.');
+                const removedContact = await removeContact(id);
+                console.log(removedContact ? 'Removed contact:' : 'Contact not found', removedContact);
+                break;
 
-      default:
-        console.warn('\x1B[31m Unknown action type!');
+            default:
+                console.warn('\x1B[31m Unknown action type!');
+        }
+    } catch (error) {
+        console.error('Error executing action:', error.message);
     }
-  } catch (error) {
-    console.error('Error executing action:', error.message);
-  }
 }
 
 invokeAction(options);
